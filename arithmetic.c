eeb7964245e3ad0c496fa2c194a43300bc4bd2cc
tanishagodhani <130463987+tanishagodhani@users.noreply.github.com>
2023-04-11 18:39:48 +0530

    Add files via upload

---
+++ arithmetic.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main()
+{
+
+	int  a=20,b=50,c;
+	c=a+b;
+	printf("%d+%d=%d\n",a,b,c);
+	c=a-b;
+	printf("%d-%d=%d\n",a,b,c);
+	c=a*b;
+	printf("%d*%d=%d\n",a,b,c);
+	c=a/b;
+	printf("%d/%d=%d\n",a,b,c);
+	c=a%b;
+	printf("a%b=%d",a,b,c);
+
+}
+
